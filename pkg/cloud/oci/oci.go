7dc02f5e878481aaf38b7b7492f4fec57a970627
Engin Diri <engin.diri@mail.schwarz>
2021-09-09 16:10:52 +0200

    Update github.com/oracle/oci-go-sdk/v47 v47.0.0
    Signed-off-by: Engin Diri <engin.diri@mail.schwarz>

--- pkg/cloud/oci/oci.go
+++ pkg/cloud/oci/oci.go
@@ -13,10 +13,10 @@ import (
 	common2 "github.com/minectl/pkg/common"
 	minctlTemplate "github.com/minectl/pkg/template"
 	"github.com/minectl/pkg/update"
-	"github.com/oracle/oci-go-sdk/v46/common"
-	"github.com/oracle/oci-go-sdk/v46/core"
-	"github.com/oracle/oci-go-sdk/v46/example/helpers"
-	"github.com/oracle/oci-go-sdk/v46/identity"
+	"github.com/oracle/oci-go-sdk/v47/common"
+	"github.com/oracle/oci-go-sdk/v47/core"
+	"github.com/oracle/oci-go-sdk/v47/example/helpers"
+	"github.com/oracle/oci-go-sdk/v47/identity"
 	"go.uber.org/zap"
 )
 
